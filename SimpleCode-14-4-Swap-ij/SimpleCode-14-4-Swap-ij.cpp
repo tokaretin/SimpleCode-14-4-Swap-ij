336e3bf6cebbdd80064a8763d8afd409f8169a8f
tokaretin <tokaretin@gmail.com>
2023-06-22 14:06:36 +0200

    Добавьте файлы проекта.

---
+++ SimpleCode-14-4-Swap-ij/SimpleCode-14-4-Swap-ij.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <vector>
+
+int main()
+{
+	float matrix[3][3];
+
+	std::cout << "Input the number: " << '\n';
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cin >> matrix[i][j];
+		}
+	}
+
+	std::cout << std::endl;
+	std::cout << "Matrix" << '\n';
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+	std::cout << "Swap matrix" << std::endl;
+	// перевернутая
+
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 1 + i; j < 3; j++)
+		{
+			float temp = matrix[i][j];
+			matrix[i][j] = matrix[j][i];
+			matrix[j][i] = temp;
+		}
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			std::cout << matrix[i][j] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+}
